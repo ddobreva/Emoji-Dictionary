d02d26225a08a72327ed8fbabfce0a6242382f33
Denitsa Dobreva <denitsa@rMB.local>
2017-06-05 18:25:25 +0200

    Emoji filled table

--- ViewController.swift
+++ ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var BasicTable: UITableView!
+    
+    var emojis = ["😮", "😋", "😏", "🤓", "😻", "🦁", "🦊", "💒", "😁", "🙊", "🦐", "🐺", "🐰"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        BasicTable.dataSource = self
+        BasicTable.delegate = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
