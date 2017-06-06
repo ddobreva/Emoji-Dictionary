4eeb9f9adf945ba9b1556af4c5c67b07a57332e7
Denitsa Dobreva <denitsa@rMB.local>
2017-06-06 15:25:00 +0200

    App is finished. Chosen emoji shows up in second screen with corresponding explanation.

--- DefinitionNewControllerViewController.swift
+++ DefinitionNewControllerViewController.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class DefinitionNewControllerViewController: UIViewController {
     
+    @IBOutlet weak var EmojiLabel: UILabel!
+    
     var emoji = "NO EMOJI"
+    
+    @IBOutlet weak var definitionLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         EmojiLabel.text = emoji
+        
+        if emoji == "😮" {
+        definitionLabel.text = "Surprised"
+        }
+        if emoji == "😋" {
+            definitionLabel.text = "Pleased"
+        }
+        if emoji == "😏" {
+            definitionLabel.text = "Kinky"
+        }
+        if emoji == "🤓" {
+            definitionLabel.text = "Clever"
+        }
+        if emoji == "😻" {
+            definitionLabel.text = "Cat in love"
+        }
+        if emoji == "🦁" {
+            definitionLabel.text = "Cute lion"
+        }
+        if emoji == "💒" {
+            definitionLabel.text = "Little church"
+        }
+        if emoji == "😁" {
+            definitionLabel.text = "Laughing"
+        }
+        if emoji == "🙊" {
+            definitionLabel.text = "I don't know anything"
+        }
+        if emoji == "🦊" {
+             definitionLabel.text = "A fox"
+        }
+        if emoji == "🦐" {
+            definitionLabel.text = "A little shrimp"
+        }
+        if emoji == "🐺" {
+            definitionLabel.text = "A scary wolf"
+        }
+        if emoji == "🐰" {
+            definitionLabel.text = "Honey bunny"
+        }
     }
     
-    @IBOutlet weak var EmojiLabel: UILabel!
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
